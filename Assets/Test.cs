9b6c42d8d436dc1bb31fd00924df7365b0bb5346
Yutaro0708 <archery0840@gmail.com>
2021-04-20 17:56:24 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 4, 6, 2, 7, 9 };
+        int i, j;
+        
+        for(i=0; i<5; i++)
+        {
+            Debug.Log(array[i]);
+        }   
+        for(j=4; j>=0; j--)
+        {
+            Debug.Log(array[j]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
