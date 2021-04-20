eab3c13fa1304573ed1d2d6fee71a6f8139efd7b
Yutaro0708 <archery0840@gmail.com>
2021-04-20 21:05:38 +0900

    ScriptTestの追記
    追記しました

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,15 +8,32 @@ public class Test : MonoBehaviour
     void Start()
     {
         int[] array = { 4, 6, 2, 7, 9 };
-        int i, j;
-        
-        for(i=0; i<5; i++)
+        int i;
+
+        for (i=0; i<5; i++)
         {
             Debug.Log(array[i]);
         }   
-        for(j=4; j>=0; j--)
+        for(i=4; i>=0; i--)
         {
-            Debug.Log(array[j]);
+            Debug.Log(array[i]);
+        }
+
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        //攻撃用の関数を呼び出す
+        lastboss.Attack();
+        //防御用の関数を呼び出す
+        lastboss.Defence(3);
+
+        Boss midboss = new Boss();
+
+        midboss.Attack();
+        midboss.Defence(7);
+        for (i = 0; i < 11; i++)
+        {
+            midboss.Magic();
         }
     }
 
@@ -26,3 +43,39 @@ public class Test : MonoBehaviour
         
     }
 }
+
+public class Boss
+{
+    private int hp = 100; //体力
+    private int power = 25;  //攻撃力
+    private int mp = 53;
+
+    //攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    //防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        //残りhpを減らす
+        this.hp -= damage;
+    }
+
+    public void Magic()
+    {
+        if (mp > 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+}
+
